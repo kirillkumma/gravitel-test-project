f049a10560e1863cc2ca49dda8d8f8fa265d156e
Kirill Kumma <kirill.kumma@gmail.com>
2022-07-14 15:19:01 +0300

    feat: add router and auth middleware to apollo

---
+++ app/urls.ts
@@ -0,0 +1,4 @@
+export enum URLs {
+  LOGIN = '/login',
+  DASHBOARD = '/dashboard',
+}
--- main.tsx
+++ main.tsx
@@ -1,19 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { BrowserRouter } from 'react-router-dom'
 
 import { App } from './app'
 import { GQL_ENDPOINT } from './config'
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: GQL_ENDPOINT,
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`,
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
-  </StrictMode>,
+  </StrictMode>
 )
